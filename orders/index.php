e1e45f69d062db06a6003284b1304c92df9ab1f7
adikgh <adiikgh@gmail.com>
2025-01-31 19:39:25 +0500

    1.0521

--- orders/index.php
+++ orders/index.php
@@ -170,11 +170,11 @@
 						<h4 class="hil_headc1 txt_c">Жеткізу қажет заказдар</h4>
 					<? endif ?>
 					<? if ($sort != 'new'): ?>
-						<div class="hil_fr1">
+						<!-- <div class="hil_fr1">
 							<div class="hil_fr1c on_sort_branch <?=($branch == 0?'hil_fr1c_act':'')?>" data-id="0">Барлығы</div>
 							<div class="hil_fr1c on_sort_branch <?=($branch == 1?'hil_fr1c_act':'')?>" data-id="1">Банзай</div>
 							<div class="hil_fr1c on_sort_branch <?=($branch == 2?'hil_fr1c_act':'')?>" data-id="2">Мастер</div>
-						</div>
+						</div> -->
 					<? endif ?>
 					<div class="hil_headc2">
 						<div class="hil_headc2s">
