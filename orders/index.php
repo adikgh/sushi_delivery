84dcb5f40ff81602b12eda29e1d4b7e90da3a805
adikgh <adiikgh@gmail.com>
2025-03-08 11:41:03 +0500

    1.0621

--- orders/index.php
+++ orders/index.php
@@ -64,7 +64,7 @@
 											<div class="uc_uil2_date1"><?=@$branch_d['name']?></div>
 											<div class=""><?=date("d-m-Y", strtotime($buy_d['ins_dt']))?> ⌛ <?=date("H:i", strtotime($buy_d['ins_dt']))?> <?=($buy_d['preorder_dt']?'| 🔴':'')?>  <?=($buy_d['preorder_dt']?$buy_d['preorder_dt']:'')?></div>
 										</div>
-										<? if ($buy_d['order_status'] == 1): ?>
+										<? if ($buy_d['order_status'] == 3): ?>
 											<div class="uc_uil2_chek">
 												<div class="btn btn_cl btn_44 on_check" data-id="<?=$buy_d['id']?>"><i class="far fa-check"></i></div>
 											</div>
